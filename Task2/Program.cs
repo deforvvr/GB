fc7f7869023ea9f5a617c2280c3800498afd3165
Egor <eeeee97345@gmail.com>
2022-12-19 18:45:32 +0300

    add new info

---
+++ Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+int max = 0;
+
+if (a > b)
+{
+    max = a;
+
+}
+else
+{
+    max = b;
+}
+
+if (max > c)
+{
+    Console.Write("Самое большое число >>> ");
+    Console.WriteLine(max);
+}
+else
+{
+    max = c;
+    Console.Write("Самое большое число >>> ");
+    Console.WriteLine(max);
+}
